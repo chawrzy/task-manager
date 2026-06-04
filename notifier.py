15201b6f09f43fab7287a2fef6411320592844c7
chawrzy <chawrzy@gmail.com>
2026-06-04 22:51:35 +0330

    adding notifier

---
+++ notifier.py
@@ -0,0 +1,25 @@
+import requests
+
+TOKEN = "BOT_TOKEN"
+CHAT_ID = "CHAT_ID"
+
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    try:
+        response = requests.post(
+            url,
+            data={
+                "chat_id": CHAT_ID,
+                "text": text
+            }
+        )
+
+        if response.status_code == 200:
+            print("Message sent ✔")
+        else:
+            print("Failed to send message ❌", response.text)
+
+    except Exception as e:
+        print("Telegram error:", e)
